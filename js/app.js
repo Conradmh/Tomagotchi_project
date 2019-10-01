4b796e23681ce538f8887b4f74cf59149e65df68
Neil <neiljessup@gmail.com>
2019-10-01 15:17:41 -0500

    basic outline

---
+++ js/app.js
@@ -0,0 +1,20 @@
+class Tomagotchi {
+  constructor: (Eat, Sleep, Play, etc),
+};
+const tomagotchi = new Tomagotchi;
+const game ={
+  //timer belongs in game
+  //lightswitch function decl here
+  // a good function ex. print(values(all values, no nuance))
+};
+
+
+
+
+
+
+//--------
+//listeners
+//no logic down here
+//only call functions
+// $('').on('click')
