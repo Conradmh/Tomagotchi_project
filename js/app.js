32e6ee56fef4e1adeae313a2e6f87a08f7dfaf1b
Neil <neiljessup@gmail.com>
2019-10-02 17:13:44 -0500

    death function added

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
 class Tomagotchi {
   constructor(name){
     this.name = name;
-    this.hunger = 0,//1-10,
-    this.sleepiness = 0, //1-10
-    this.boredom = 0, //1-10
+    this.hunger = 1,//1-10,
+    this.sleepiness = 1, //1-10
+    this.boredom = 1, //1-10
     this.age = 0
   }
 
 };
 
 const game ={
-  time: 0,
+  time: 30,
   pet: null,
   //timer belongs in game
   //only one timer in game, no multiple setIntervals
   //lightswitch function decl here
   // a good function ex. print(values(all values, no nuance))
-  start: function(){
+  startTimer(){
+        const $timer = $('#timer');
+        const interval = setInterval(() => {
+          if (this.time !== 0){
+            this.time--;
+            this.tomagotchiDeathInc();
+          } else {
+              clearInterval(interval);
+          }
+            $timer.text(`timer: ${this.time}`)
+
+      }, 1000)
+
+  },
+
+  start(){
     const tomagotchi = new Tomagotchi($('#inputBox').val());
     this.pet = tomagotchi;
-
-
-  //  console.log(tomagotchiName, 'before');
-    // this.pet = tomagotchiName;
     console.log(this.pet, 'last');
+    this.killTomagatchi();
+    this.startTimer();
+  },
 
- }
+  killTomagatchi(){
+    if (this.pet.hunger === 10 || this.pet.sleepiness === 10 ||     this.pet.boredom === 10) {
+      $('#tomagotchi').hide();
+    }
+  },
+
+  tomagotchiDeathInc(){
+    this.pet.hunger++;
+    this.pet.sleepiness++;
+    this.pet.boredom++;
+  }
 }
-
 
 
 //--------
@@ -43,7 +66,14 @@ $('form').on('submit', (e) => {
   $('inputBox').html();
   game.start();
 });
-$('#start').on('click', () => {
-  game.start();
-  $('#start').hide();
-});
+
+// $('#start').on('click', () => {
+//   game.start();
+//   $('#start').hide();
+// });
+
+// tomagotchiDeathInc: function(){
+//   this.hunger++;
+//   this.sleepiness++;
+//   this.boredom++;
+// }
