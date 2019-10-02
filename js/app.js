5a8f5e7f99489a9588741330f26a1ff86efd6145
Neil <neiljessup@gmail.com>
2019-10-02 14:47:22 -0500

    instantiated class and added display of properties

--- js/app.js
+++ js/app.js
@@ -7,37 +7,26 @@ class Tomagotchi {
     this.age = 0
   }
 
-  changeName(){
-    tomagotchi.name = prompt(`Please name your new little creature`);
-    return tomagotchi.name;
-  }
-//   myMove: function() {
-//   var elem = document.getElementById("animate");
-//   var pos = 0;
-//   var id = setInterval(frame, 5);
-//   function frame() {
-//     if (pos == 350) {
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       elem.style.top = pos + 'px';
-//       elem.style.left = pos + 'px';
-//     }
-//   }
-// }
-
 };
-const tomagotchi = new Tomagotchi(/*name*/);
+
 const game ={
   time: 0,
+  pet: null,
   //timer belongs in game
   //only one timer in game, no multiple setIntervals
   //lightswitch function decl here
   // a good function ex. print(values(all values, no nuance))
-};
+  start: function(){
+    const tomagotchi = new Tomagotchi($('#inputBox').val());
+    this.pet = tomagotchi;
+    // this.pet.name = $('#inputBox').val();
 
+  //  console.log(tomagotchiName, 'before');
+    // this.pet = tomagotchiName;
+    console.log(this.pet, 'last');
 
-
+ }
+}
 
 
 
@@ -47,16 +36,11 @@ const game ={
 //only call functions
 // $('').on('click')
 
-$('#start').on('click', (e) => {
-  tomagotchi.changeName();
-  console.log(tomagotchi.name);
-});
-
-$('form' /*#submitBut*/).on('submit', (e) => {
-  tomagotchi.name = $('#inputBox').val();
-  console.log(tomagotchi.name);
-  //console.log('clicked');
-  //console.log( $('#inputBox').val());
+$('form').on('submit', (e) => {
+  $('#inputBox').val()
   e.preventDefault();
   $('form').hide();
+  $('inputBox').html();
+  game.start();
+
 });
