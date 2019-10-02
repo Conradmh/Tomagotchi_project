11197a5efefafac96eb2a9d00b0551b731881a27
Neil <neiljessup@gmail.com>
2019-10-02 10:08:36 -0500

    base tomogatchi div created, three action buttons added

--- js/app.js
+++ js/app.js
@@ -3,17 +3,32 @@ class Tomagotchi {
     this.eat = eat;
     this.sleep = sleep;
     this.play = play;
-
   }
   name: 'nagato',
   hunger: 1,//1-10,
   sleepiness: 1, //1-10
   boredom: 1, //1-10
-  age:  0,
+  age:  0
+
+  myMove: function() {
+  var elem = document.getElementById("animate");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
 
 };
 const tomagotchi = new Tomagotchi;
 const game ={
+  
   //timer belongs in game
   //only one timer in game, no multiple setIntervals
   //lightswitch function decl here
