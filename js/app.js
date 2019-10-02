849d28a0730020b501ed6359458028439f9532c4
Neil <neiljessup@gmail.com>
2019-10-02 09:43:11 -0500

    Tamagotchi class created and instantiated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 class Tomagotchi {
-  constructor: (Eat, Sleep, Play, etc),
+  constructor: (eat, sleep, play){
+    this.eat = eat;
+    this.sleep = sleep;
+    this.play = play;
+
+  }
+  name: 'nagato',
+  hunger: 1,//1-10,
+  sleepiness: 1, //1-10
+  boredom: 1, //1-10
+  age:  0,
+
 };
 const tomagotchi = new Tomagotchi;
 const game ={
   //timer belongs in game
+  //only one timer in game, no multiple setIntervals
   //lightswitch function decl here
   // a good function ex. print(values(all values, no nuance))
 };
