65d44c384e7030b7a23f4651dd0ce9da44df5b5d
Neil <neiljessup@gmail.com>
2019-10-03 09:35:54 -0500

    basic game functions working

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ class Tomagotchi {
 };
 
 const game ={
-  time: 30,
+  time: 60,
   pet: null,
   //timer belongs in game
   //only one timer in game, no multiple setIntervals
@@ -19,15 +19,17 @@ const game ={
   startTimer(){
         const $timer = $('#timer');
         const interval = setInterval(() => {
-          if (this.time !== 0){
-            this.time--;
-            this.tomagotchiDeathInc();
-          } else {
-              clearInterval(interval);
-          }
-            $timer.text(`timer: ${this.time}`)
 
-      }, 1000)
+          $timer.text(`timer: ${this.time}`)
+
+          this.tomagotchiDeathInc();
+          this.updateStats();
+          this.time--;
+          this.pet.age += 0.5;
+          this.killTomagatchi(interval);
+          this.lightsOn();
+          this.winState();
+      }, 1200)
 
   },
 
@@ -35,20 +37,69 @@ const game ={
     const tomagotchi = new Tomagotchi($('#inputBox').val());
     this.pet = tomagotchi;
     console.log(this.pet, 'last');
-    this.killTomagatchi();
     this.startTimer();
   },
 
-  killTomagatchi(){
-    if (this.pet.hunger === 10 || this.pet.sleepiness === 10 ||     this.pet.boredom === 10) {
+  killTomagatchi(interval){
+    if ((this.pet.hunger >= 10) || (this.pet.sleepiness >= 10) ||     (this.pet.boredom >= 10)) {
       $('#tomagotchi').hide();
+      clearInterval(interval);
     }
   },
 
   tomagotchiDeathInc(){
-    this.pet.hunger++;
-    this.pet.sleepiness++;
-    this.pet.boredom++;
+    // while (this.pet.hunger < 10 || this.pet.sleepiness < 10 ||     this.pet.boredom < 10) {
+    //   if (this.time % 2 === 0) {
+        this.pet.hunger++;
+        this.pet.sleepiness++;
+        this.pet.boredom++;
+      // }
+    //}
+  },
+  winState(interval){
+    const $win = $('#win');
+    if (game.time === 0) {
+      clearInterval(interval);
+      $win.html(`You won!`);
+    }
+  },
+  lightsOn(interval){
+    if (this.pet.sleepiness == 4) {
+      $('body').css('background-color', 'yellow')
+    }
+  },
+  lightsOff(){
+
+  //$('body').css('background-color', 'blue');
+    this.pet.sleepiness = 1;
+  },
+  fillBelly(){
+    //  const $feed = $('#feed');
+    this.pet.hunger -= 2 ;
+  },
+  throwBone(){
+    //  const $excite = $('#excite');
+    this.pet.boredom -= 3;
+  },
+  evolve(){
+    if(this.pet.age == 8){
+
+    }
+  },
+  updateStats(){
+    const $hunger = $('#hunger');
+    const $sleepiness = $('#sleepiness');
+    const $boredom = $('#boredom');
+    const $age = $('#age');
+    // $this.pet.boredom++;
+    // $this.pet.sleepiness++;
+    // $this.pet.hunger = this.pet.hunger + 2;
+
+    $hunger.text(`${this.pet.name}s Hunger: ${this.pet.hunger}`)
+    $sleepiness.text(`${this.pet.name}s Sleepiness: ${this.pet.sleepiness}`)
+    $boredom.text(`${this.pet.name}s Boredom: ${this.pet.boredom}`)
+    $age.text(`${this.pet.name}s Age: ${this.pet.age}`)
+
   }
 }
 
@@ -67,11 +118,20 @@ $('form').on('submit', (e) => {
   game.start();
 });
 
-// $('#start').on('click', () => {
-//   game.start();
-//   $('#start').hide();
-// });
+$('#rest').on('click', () => {
+  console.log('this rest button works')
+  game.lightsOff();
+ $('body').css('background-color', 'blue')
+});
 
+$('#feed').on('click', () => {
+  game.fillBelly();
+
+});
+$('#excite').on('click', () => {
+  game.throwBone();
+
+});
 // tomagotchiDeathInc: function(){
 //   this.hunger++;
 //   this.sleepiness++;
